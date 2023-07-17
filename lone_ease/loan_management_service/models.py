f8b0ca4ec4413b7184d77a7f64a6d24330cf94b4
Akshat Gupta <akshatguptamac@gmail.com>
2023-07-18 03:05:19 +0530

    created a user information model

--- lone_ease/loan_management_service/models.py
+++ lone_ease/loan_management_service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class UserInformation(models.Model):
+
+    TRANSACTION_TYPES = [
+        ('CREDIT', 'Credit'),
+        ('DEBIT', 'Debit')
+    ]
+
+    user_id = models.TextField(max_length=100)
+    date = models.DateTimeField(auto_now_add=False)
+    amount = models.FloatField(default=0.0)
+    transaction_type = models.CharField(max_length=8, choices=TRANSACTION_TYPES)
+    
